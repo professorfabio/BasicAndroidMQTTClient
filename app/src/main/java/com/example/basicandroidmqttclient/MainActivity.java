3232b081a3830af688fec32f724b3a6a33890dd2
Fábio M. Costa <fmc@ufg.br>
2023-11-24 00:24:17 -0300

    Minimal working version (with publisher and subscriber)

--- app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
@@ -16,6 +17,8 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.basicandroidmqttclient.MESSAGE";
+    public static final String brokerURI = "3.223.10.115";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,16 +33,38 @@ public class MainActivity extends AppCompatActivity {
 
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
-                .serverHost("3.223.10.115")
+                .serverHost(brokerURI)
                 .buildBlocking();
 
         client.connect();
         client.publishWith().topic(topicName.getText().toString()).qos(MqttQos.AT_LEAST_ONCE).payload(value.getText().toString().getBytes()).send();
         client.disconnect();
 
-
         String message = topicName.getText().toString() + " " + value.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void sendSubscription(View view) {
+        EditText topicName = (EditText) findViewById(R.id.editTextTopicNameSub);
+
+        Mqtt5BlockingClient client = Mqtt5Client.builder()
+                .identifier(UUID.randomUUID().toString())
+                .serverHost(brokerURI)
+                .buildBlocking();
+
+        client.connect();
+
+        client.toAsync().subscribeWith()
+                .topicFilter(topicName.getText().toString())
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .callback(System.out::println)
+                .send();
+    }
+
+    public void subscriptionCallback(String msg) {
+        TextView textView = (TextView) findViewById(R.id.textViewSubscribedMsg);
+
+        textView.setText(msg);
     }
 }
